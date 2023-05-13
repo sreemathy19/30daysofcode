1ce4fbc90bd1cd44fdd6db8bb93d0cb3b9a71f21
Sreemathy <112180633+sreemathy19@users.noreply.github.com>
2023-05-13 14:17:40 +0530

    Create DAY0.java

---
+++ code/DAY0.java
@@ -0,0 +1,12 @@
+//Problem Statement-[Hello, World](https://www.hackerrank.com/challenges/30-hello-world)
+import java.io.*;
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		String inputString = scan.nextLine(); 
+		scan.close(); 
+		System.out.println("Hello, World.");
+    System.out.println(inputString);
+	}
+}
